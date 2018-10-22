7577393959e908183339b4b858047c879048b573
Sonaalk <30656971+Sonaalk@users.noreply.github.com>
2018-10-23 01:25:38 +0530

    Create register.php

---
+++ register.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Query</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <div class="header">
+  	<h2>User Query</h2>
+  </div>
+	 
+  <form method="post" action="login.php">
+  	
+  	<div class="input-group">
+  		<label>Enter the Roll NO.</label>
+  		<input type="text" name="rollno" >
+  	</div>
+  	
+  	<div class="input-group">
+  		<button type="submit" class="btn" name="login_user">Login</button>
+  	</div>
+  	
+  </form>
+</body>
+</html>
